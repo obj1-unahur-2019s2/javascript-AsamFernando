168dcc9c102bc007a10aa6ebe6b49ca69f5ad33e
AsamFernando <ferasam88@gmail.com>
2019-11-21 21:09:59 -0300

    practica de js pepita

--- pepita/pepita.js
+++ pepita/pepita.js
@@ -1,3 +1,6 @@
+
+
+
 class Golondrina {
     constructor() {
         this._energia = 0;
@@ -22,10 +25,23 @@ const mondongo = { energiaPorGramo: () => 100 }
 
 
 function darleComidaAPepita() {
-    pepita.comer(alpiste, 20);
+    pepita.comer(alpiste, document.getElementById("cantAlp").value);
+    const label = document.getElementById("energia");//se trae el id energia del html y cambia el valor q tiene por la energia actualizada
+    label.innerText = pepita.energia();
+}
+function comerMondongo() {
+    pepita.comer(mondongo, document.getElementById("cantMon").value);
+    const label = document.getElementById("energia")
+    label.innerText = pepita.energia();
+}
+
+function pepitaVolar() {
+    pepita.volar(document.getElementById("volarKms").value)
     const label = document.getElementById("energia");
     label.innerText = pepita.energia();
 }
+
+function imagenPepita()
 
 const pepita = new Golondrina();
 
